01a1c380ced7f88852f00913cfe004a133fd4088
esennesh <elisennesh@gmail.com>
2020-06-05 18:28:16 -0400

    base_model.BaseModel: add resume-from-checkpoint method
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- base/base_model.py
+++ base/base_model.py
@@ -24,3 +24,13 @@ class BaseModel(pyro.nn.PyroModule):
         model_parameters = filter(lambda p: p.requires_grad, self.parameters())
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
+
+    def resume_from_checkpoint(self, resume_path):
+        """
+        Resume from saved checkpoints
+
+        :param resume_path: Checkpoint path to be resumed
+        """
+        resume_path = str(resume_path)
+        checkpoint = torch.load(resume_path)
+        self.load_state_dict(checkpoint['state_dict'])
